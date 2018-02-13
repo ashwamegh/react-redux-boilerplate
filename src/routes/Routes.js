4139a7335e576c8f56320d702228c5d16f642cee
Shashank Shekhar <shashank7200@gmail.com>
2018-02-13 13:21:55 +0530

    refractored the routes component

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,14 +1,24 @@
 //  Global imports
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Switch, Redirect } from 'react-router';
+import { Component } from 'react';
 
 // Local imports
 import App from './../App';
 
 
-export default (
-  <Switch>
-    <Route exact path='/' component={App}/>
-    <Redirect to="/"/>
-  </Switch>
-)
+class Routes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route
+          component={App}
+          exact
+          path="/"
+        />
+        <Redirect to="/"/>
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
